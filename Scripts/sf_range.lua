6cd323433f56dab7bb8fe69068c705dbccef1afc
rusboys <Lions.will2014@gmail.com>
2014-06-14 14:48:46 +0700

    Update sf_range.lua

--- Scripts/sf_range.lua
+++ Scripts/sf_range.lua
@@ -1,29 +1,46 @@
-Range1 = 200
-Range2 = 450
-Range3 = 700
+sleep = 10
 
-function Checkname( name )
-	if me.name == "Nevermore" then
-		DrawRange()
+sleeptick = nil
+coils = {}
+
+function Tick( tick )
+	if not client.connected or client.loading or client.console or not entityList:GetMyHero() or (sleeptick and tick < sleeptick + sleep ) then
+		return		
 	end
-end
 
-function GameStart()
-	DrawRange()
-end
+	me = entityList:GetMyHero()
 
-function DrawRange()
-			local eff = Effect(me,"range_display")
-			local eff1 = Effect(me,"range_display")
-			local eff2 = Effect(me,"range_display")
-			eff:SetParameter( Vector(Range1,0,0) )
-			eff1:SetParameter( Vector(Range2,0,0) )
-			eff2:SetParameter( Vector(Range3,0,0) )
+	if me.name ~= "npc_dota_hero_nevermore" then				
+		return
+	else
+	c_abs = {}
+	table.insert(c_abs, me:GetAbility(1))	
+	table.insert(c_abs, me:GetAbility(2))
+	table.insert(c_abs, me:GetAbility(3))
+	end
+	
+	R = {200, 450, 700}	
+	alfa = me.rotR
+	for i=1,3 do	
+		local p = Vector(me.position.x + R[i] * math.cos(alfa), me.position.y + R[i] * math.sin(alfa), me.position.z)
+				
+		if not coils[i] then					
+			if c_abs[i] and c_abs[i].state == -1 then
+				coils[i] = Effect(p,  "range_display")
+				coils[i]:SetVector(1, Vector(250,0,0) )
+				coils[i]:SetVector(0, p )				
+			end
+		else
+			if c_abs[i] and c_abs[i].state == - 1 then
+				coils[i]:SetVector(0, p )	
+			else
+				coils[i] = nil
+				collectgarbage("collect")
+			end			
 		end
-
-
-script:RegisterEvent(EVENT_START,GameStart)
-
-if me then
-	DrawRange()
+	end			
+	
+	sleeptick = tick
 end
+
+script:RegisterEvent(EVENT_FRAME, Tick)
